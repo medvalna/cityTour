7e8cb11ab67f788082dc4cae3e094f5c7cb93bca
Burakova Valentina <v.m.30.03.20@gmail.com>
2025-04-02 00:08:34 +0300

    fix: decomposition

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import './Select.css'
 import { onMounted, onUnmounted, ref } from 'vue'
 import type { City } from '@/types/city'
 import ArrowDown from '@/assets/icons/ArrowDown.svg'
@@ -18,6 +19,7 @@ const isOpen = ref(false)
 const searchText = ref('')
 const filteredCities = ref<City[]>(props.cities)
 const selectWrapper = ref<HTMLElement | null>(null)
+
 const toggleDropdown = () => {
   isOpen.value = !isOpen.value
   searchText.value = ''
@@ -47,6 +49,7 @@ const filterCities = () => {
     )
   }
 }
+
 const handleClickOutside = (event: MouseEvent) => {
   if (selectWrapper.value && !selectWrapper.value.contains(event.target as Node)) {
     isOpen.value = false
@@ -100,105 +103,3 @@ defineExpose({
     </ul>
   </div>
 </template>
-
-<style>
-.buttons {
-  display: flex;
-  flex-direction: row;
-  gap: 2px;
-}
-.selectWrapper {
-  width: 300px;
-  position: relative;
-  border: 1px solid var(--custom-gray);
-}
-.selectWrapper:hover {
-  border-color: var(--custom-yellow);
-  outline: none;
-}
-.selector {
-  width: 300px;
-  height: 50px;
-}
-.selectedText {
-  color: black;
-}
-.placeholder {
-  cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  color: var(--text-gray);
-  border-radius: 1px;
-  padding: 14.5px 15px;
-}
-.placeholder img {
-  transition: transform 0.3s ease;
-}
-.rotate-180 {
-  transform: rotate(180deg);
-}
-
-.list {
-  width: 300px;
-  max-height: 239px;
-  background-color: white;
-  border-radius: 1px;
-  overflow-x: hidden;
-  overflow-y: scroll;
-  padding: 0;
-  position: absolute;
-  z-index: 1;
-  border: 1px solid var(--custom-gray);
-}
-
-.options {
-  border: 1px solid var(--custom-gray);
-  border-radius: 1px;
-  position: relative;
-
-  width: 100%;
-  padding: 12.5px 15px;
-  cursor: pointer;
-  list-style: none;
-  color: var(--text-gray);
-}
-.options:hover {
-  background-color: var(--custom-yellow);
-  color: black;
-}
-
-.search {
-  width: 100%;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-}
-.inputBox {
-  margin: 5px;
-  width: 90%;
-  padding: 10px;
-  outline: none;
-  border: 1px solid var(--custom-gray);
-  border-radius: 6px;
-  display: flex;
-  justify-content: space-between;
-}
-.search input {
-  outline: none;
-  border: none;
-  width: 100%;
-}
-.inputBox:hover {
-  border: 1px solid var(--custom-yellow);
-}
-.inputBox:focus {
-  border: 1px solid var(--custom-yellow);
-}
-.noResults {
-  display: flex;
-  justify-content: center;
-  padding: 14.5px 15px;
-}
-</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+import './MainPage.css'
+import { onMounted, ref, watch } from 'vue'
+import Input from '@/components/Input.vue'
+import Select from '@/components/Select/Select.vue'
+import TourCard from '@/components/TourCard.vue'
+import SputnikLogo from '@/components/SputnikLogo.vue'
+import type { Tour } from '@/types/tour'
+import { fetchTours } from '@/api/fetchTours'
+import { fetchCities } from '@/api/fetchCities'
+import type { City } from '@/types/city'
+
+const tours = ref<Tour[]>([])
+const cities = ref<City[]>([])
+const filteredTours = ref<Tour[]>([])
+const selectedCity = ref<City | null>(null)
+const tourName = ref<string>('')
+
+const inputRef = ref<InstanceType<typeof Input> | null>(null)
+const multiSelectRef = ref<InstanceType<typeof Select> | null>(null)
+
+const filterToursByCity = () => {
+  if (!selectedCity.value) {
+    filteredTours.value = tours.value
+    return
+  }
+
+  filteredTours.value = tours.value.filter((tour) => tour.city_id === selectedCity.value?.id)
+}
+const filterToursByName = () => {
+  if (!tourName.value) {
+    filteredTours.value = tours.value
+    return
+  }
+
+  filteredTours.value = tours.value.filter((tour) =>
+    tour.title.toLowerCase().includes(tourName.value.toLowerCase()),
+  )
+}
+
+watch(selectedCity, () => {
+  filterToursByCity()
+})
+
+watch(tourName, () => {
+  filterToursByName()
+})
+
+const handleCityUpdate = (city: City | null) => {
+  selectedCity.value = city
+}
+const handleTourNameUpdate = (tour: string) => {
+  tourName.value = tour
+}
+
+const deleteFilters = () => {
+  inputRef.value?.clear()
+  multiSelectRef.value?.clear()
+  handleCityUpdate(null)
+  handleTourNameUpdate('')
+}
+
+onMounted(async () => {
+  const [toursData, citiesData] = await Promise.all([fetchTours(), fetchCities()])
+
+  tours.value = toursData
+  cities.value = citiesData
+  filteredTours.value = toursData
+})
+</script>
+<template>
+  <div class="container">
+    <div class="title">
+      <SputnikLogo />
+      <h1>Экскурсии по всему миру</h1>
+      <div class="form">
+        <Input ref="inputRef" @update:tour="handleTourNameUpdate" />
+        <Select
+          ref="multiSelectRef"
+          v-bind:cities="cities"
+          v-bind="selectedCity"
+          @update:city="handleCityUpdate"
+        />
+      </div>
+    </div>
+    <div v-if="filteredTours" class="cards">
+      <div v-for="(tour, index) in filteredTours" :key="index">
+        <TourCard v-bind:tour="tour" />
+      </div>
+    </div>
+    <div v-if="filteredTours.length === 0" class="noTours">
+      <p>Поиск не дал результатов</p>
+      <button @click="deleteFilters">Сбросить фильтры</button>
+    </div>
+  </div>
+</template>
